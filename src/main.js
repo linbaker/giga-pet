f46e3008353c1977492616c677fa364574cfcb77
kaya jepson and lindsey baker <kaya@>
2019-06-03 13:52:46 -0700

    Add entry point

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { Dog } from './functions.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function(){
+  console.log("test");
+  });
